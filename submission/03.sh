dfc477bc775d0c8eb4cba0692ce8af91aacddd6b
yhzlsm <yhzlsm@gmail.com>
2025-03-21 19:47:36 -0300

    fix: return only the address

--- submission/03.sh
+++ submission/03.sh
@@ -1,6 +1,6 @@
 # Created a SegWit address.
 ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32m)
 # Add funds to the address.
-bitcoin-cli -regtest generatetoaddress 101 $ADDRESS
+FUNDS=$(bitcoin-cli -regtest generatetoaddress 101 $ADDRESS)
 # Return only the Address
 echo $ADDRESS
