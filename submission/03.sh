26fe8e98d6524abf6066f867045fcd3d3fb9b210
yhzlsm <yhzlsm@gmail.com>
2025-03-21 19:29:26 -0300

    fix: change to taproot address

--- submission/03.sh
+++ submission/03.sh
@@ -1,5 +1,5 @@
 # Created a SegWit address.
-ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
+ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32m)
 # Add funds to the address.
 bitcoin-cli -regtest generatetoaddress 101 $ADDRESS
 # Return only the Address
